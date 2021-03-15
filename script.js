189a1a21a7942d9f41c63c08ef2301fcec27cc6a
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-15 17:28:02 +0200

    working on functionality, fetching through temp proxy api

---
+++ script.js
@@ -0,0 +1,27 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorName = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// Get quote from API
+const getQuote = async () => {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/"; //this is proxy api, it's for development purposes only so use your own
+  const apiUrl = "http://api.forismatic.com/api/1.0/";
+  const queryString = "?method=getQuote&lang=en&format=json";
+  const requestUrl = apiUrl + queryString;
+  try {
+    // we will use the proxy api to call the api we want to bypass the cors plicy error
+    const response = await fetch(proxyUrl + requestUrl);
+    const data = await response.json();
+    // console.log(data);
+    authorName.innerText = data.quoteAuthor;
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    getQuote(); //in case an error happens, we want to try and fetch another quote
+    console.log("Opsy dops, something went wrong..", error);
+  }
+};
+
+// On Load
+getQuote();
