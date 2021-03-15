41b17ba4714eaf3b3e8dced1552bcf4da5505322
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-15 18:02:39 +0200

    added loading animation

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const quoteText = document.getElementById("quote");
 const authorName = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Hide loading
+const complete = () => {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+};
 
 // Get quote from API
 const getQuote = async () => {
+  // Show loader before doing anything.
+  loading();
   const proxyUrl = "https://cors-anywhere.herokuapp.com/"; //this is proxy api, it's for development purposes only so use your own
   const apiUrl = "http://api.forismatic.com/api/1.0/";
   const queryString = "?method=getQuote&lang=en&format=json";
@@ -28,6 +45,8 @@ const getQuote = async () => {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    // Stop loader, show quote
+    complete();
   } catch (error) {
     // getQuote(); //in case an error happens, we want to try and fetch another quote
     console.log("Opsy dops, something went wrong..", error);
@@ -35,12 +54,12 @@ const getQuote = async () => {
 };
 
 // Tweet quote
-function tweetQuote() {
+const tweetQuote = () => {
   const quote = quoteText.innerText;
   const author = authorName.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(twitterUrl, "_blank");
-}
+};
 
 // Event listeners
 newQuoteBtn.addEventListener("click", getQuote);
